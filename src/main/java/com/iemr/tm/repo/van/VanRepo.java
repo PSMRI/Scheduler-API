c67a68ca7c033001e1da6f9981b7b68b70e6477f
indraniBan <133205641+indraniBan@users.noreply.github.com>
2024-01-24 11:59:56 +0530

    json-path vulnerabilities fixed (#31)
    * version upgradation changes
    
    * version upgradation changes
    
    * resolved all the errors for upgradation
    
    * vulnerabilities fixed
    
    * Issues fixed
    
    * vulnerabilities fixed
    
    * vulnerabilities fixed
    
    * vulnerabilities fixed
    
    ---------
    
    Co-authored-by: Indrani Bandyopadhyay <IN40068837@wipro.com>
    Co-authored-by: ravishanigarapu <133210792+ravishanigarapu@users.noreply.github.com>

--- src/main/java/com/iemr/tm/repo/van/VanRepo.java
+++ src/main/java/com/iemr/tm/repo/van/VanRepo.java
@@ -22,13 +22,11 @@
 package com.iemr.tm.repo.van;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.iemr.tm.data.van.MVan;
 
 @Repository
-@RestResource(exported = false)
 public interface VanRepo extends JpaRepository<MVan, Integer> {
 	
 	MVan findByVanID(Integer vanID);
