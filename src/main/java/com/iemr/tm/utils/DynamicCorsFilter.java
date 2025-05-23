f956d311f8a8fb2ab7e529c4ef8a7b50f89f1168
vishwab1 <vishwanath@navadhiti.com>
2025-05-23 18:44:53 +0530

    fix(cors): cors origin added

--- src/main/java/com/iemr/tm/utils/DynamicCorsFilter.java
+++ src/main/java/com/iemr/tm/utils/DynamicCorsFilter.java
@@ -1,15 +1,16 @@
-package com.iemr.common.utils;
+package com.iemr.tm.utils;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.Arrays;
 
 @Component
 public class DynamicCorsFilter extends OncePerRequestFilter {
