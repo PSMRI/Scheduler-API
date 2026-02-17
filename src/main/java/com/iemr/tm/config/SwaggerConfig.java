52f78145f46ca493dbfcf7c424f22e6370871c15
KOPPIREDDY DURGA PRASAD <144464542+DurgaPrasad-54@users.noreply.github.com>
2026-02-17 16:32:40 +0530

    Add new workflow and server urls (#64)
    * Add DeepWiki badge to README
    
    Added a badge for DeepWiki to the README.
    
    * chore(swagger): automate swagger sync to amrit-docs
    
    * chore(swagger): automate swagger sync to amrit-docs
    
    * chore(swagger): automate swagger sync to amrit-docs
    
    * chore(swagger): automate swagger sync to amrit-docs
    
    * chore(swagger): update job kills process
    
    * chore(swagger): add jq installation in swagger-json.yml
    
    * chore(swagger): add delete branch after merge
    
    * chore(swagger): remove one fi from the swagger workflow
    
    * chore(swagger): add env-driven Dev/UAT/Demo servers and update workflow
    
    * chore(swagger): add new description for swagger doc
    
    * chore(swagger): add version for swagger doc
    
    * chore(swagger): add constant for bearerAuth
    
    * chore(swagger): add swagger profile include in properties
    
    * chore(swagger): swagger profile include removed from the properties

--- src/main/java/com/iemr/tm/config/SwaggerConfig.java
+++ src/main/java/com/iemr/tm/config/SwaggerConfig.java
@@ -2,23 +2,37 @@ package com.iemr.tm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Arrays;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
-	
-	@Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new 
-             Info().title("Scheduler API").version("version").description("A microservice for the creation and management of beneficiaries."))
-                .addSecurityItem(new SecurityRequirement().addList("my security"))
-                .components(new Components().addSecuritySchemes("my security",
-                        new SecurityScheme().name("my security").type(SecurityScheme.Type.HTTP).scheme("bearer")));
+    private static final String DEFAULT_SERVER_URL = "http://localhost:9090";
+    private static final String BEARER_AUTH = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI(Environment env) {
+        String devUrl = env.getProperty("api.dev.url", DEFAULT_SERVER_URL);
+        String uatUrl = env.getProperty("api.uat.url", DEFAULT_SERVER_URL);
+        String demoUrl = env.getProperty("api.demo.url", DEFAULT_SERVER_URL);
+        return new OpenAPI()
+            .info(new Info().title("Scheduler API").version("1.0.0").description("A microservice for scheduling and appointment management."))
+            .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
+            .components(new Components().addSecuritySchemes(BEARER_AUTH,
+                new SecurityScheme().name(BEARER_AUTH).type(SecurityScheme.Type.HTTP).scheme("bearer")))
+            .servers(Arrays.asList(
+                new Server().url(devUrl).description("Dev"),
+                new Server().url(uatUrl).description("UAT"),
+                new Server().url(demoUrl).description("Demo")
+            ));
     }
 
 }
